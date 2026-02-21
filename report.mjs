df64207d727561f5ac45ef8b4ae6dfbd2ae38dda
Erik Soehnel <erik.soehnel@gmail.com>
2026-02-21 10:33:37 +0100

    parse broken jsonl lines

--- report.mjs
+++ report.mjs
@@ -25,9 +25,63 @@ async function getSessionFiles(dir) {
   return files.flat().filter((file) => file !== null);
 }
 
+// somehow the pi agents session jsonl logfiles sometimes have two json objects on the same line, breaking a simple readline-parsejson loop
+// this simple state machine runs over the jsonl file string and cleans that up
+function formatJsonString(jsonString) {
+  const states = {
+    OUTSIDE_OBJECT: "OUTSIDE_OBJECT",
+    INSIDE_OBJECT: "INSIDE_OBJECT",
+    INSIDE_STRING: "INSIDE_STRING",
+  };
+
+  let currentState = states.OUTSIDE_OBJECT;
+  let braceLevel = 0;
+  let currentObject = "";
+  const jsonObjects = [];
+
+  for (let i = 0; i < jsonString.length; i++) {
+    const char = jsonString[i];
+
+    switch (currentState) {
+      case states.OUTSIDE_OBJECT:
+        if (char === "{") {
+          currentState = states.INSIDE_OBJECT;
+          braceLevel++;
+          currentObject += char;
+        }
+        break;
+
+      case states.INSIDE_OBJECT:
+        currentObject += char;
+        if (char === '"') {
+          currentState = states.INSIDE_STRING;
+        } else if (char === "{") {
+          braceLevel++;
+        } else if (char === "}") {
+          braceLevel--;
+          if (braceLevel === 0) {
+            currentState = states.OUTSIDE_OBJECT;
+            jsonObjects.push(currentObject);
+            currentObject = "";
+          }
+        }
+        break;
+
+      case states.INSIDE_STRING:
+        currentObject += char;
+        if (char === '"' && jsonString[i - 1] !== "\\") {
+          currentState = states.INSIDE_OBJECT;
+        }
+        break;
+    }
+  }
+
+  return jsonObjects.join("\n");
+}
+
 async function processSessionFile(filePath) {
   const rawContent = fs.readFileSync(filePath).toString();
-  const sanitized = rawContent.replaceAll("}{", "}\n{");
+  const sanitized = formatJsonString(rawContent);
 
   const costsByModel = new Map();
 
